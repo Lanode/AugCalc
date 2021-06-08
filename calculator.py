5cce48eb9f054b162d4f1fc89be7d13a7a9ffc32
Lanode <lar4encko.mischa@gmail.com>
2021-06-08 09:35:48 +0300

    Release 1.4 + Calculator now can take keyword arguments for variables

--- calculator.py
+++ calculator.py
@@ -1,44 +1,56 @@
+from math_parser import parse
 from math_ast import *
 import math
 
-def calculate(ast: MathAST) -> float:
-    if isinstance(ast, Constant):
-        return ast.value
-    elif isinstance(ast, Pi):
-        return math.pi
-    elif isinstance(ast, Exponenta):
-        return math.e
-    elif isinstance(ast, Sin):
-        return math.sin(calculate(ast.a))
-    elif isinstance(ast, Cos):
-        return math.cos(calculate(ast.a))
-    elif isinstance(ast, Tg):
-        return math.tan(calculate(ast.a))
-    elif isinstance(ast, Ctg):
-        return math.cos(calculate(ast.a))/math.sin(calculate(ast.a))
-    elif isinstance(ast, Ln):
-        return math.log10(calculate(ast.a))
-    elif isinstance(ast, Usub):
-        return -calculate(ast.a)
-    elif isinstance(ast, Arcsin):
-        return math.asin(calculate(ast.a))
-    elif isinstance(ast, Arccos):
-        return math.acos(calculate(ast.a))
-    elif isinstance(ast, Arctg):
-        return math.atan(1/calculate(ast.a))
-    elif isinstance(ast, Sqrt):
-        return math.sqrt(calculate(ast.a))
+def calculate(ast: MathAST, **kwargs) -> float:
+    def calc(ast: MathAST) -> float:
+        if isinstance(ast, Constant):
+            return ast.value
+        elif isinstance(ast, Pi):
+            return math.pi
+        elif isinstance(ast, Exponenta):
+            return math.e
+        elif isinstance(ast, Sin):
+            return math.sin(calc(ast.a))
+        elif isinstance(ast, Cos):
+            return math.cos(calc(ast.a))
+        elif isinstance(ast, Tg):
+            return math.tan(calc(ast.a))
+        elif isinstance(ast, Ctg):
+            return math.cos(calc(ast.a))/math.sin(calc(ast.a))
+        elif isinstance(ast, Ln):
+            return math.log10(calc(ast.a))
+        elif isinstance(ast, Usub):
+            return -calc(ast.a)
+        elif isinstance(ast, Arcsin):
+            return math.asin(calc(ast.a))
+        elif isinstance(ast, Arccos):
+            return math.acos(calc(ast.a))
+        elif isinstance(ast, Arctg):
+            return math.atan(1/calc(ast.a))
+        elif isinstance(ast, Sqrt):
+            return math.sqrt(calc(ast.a))
 
-    elif isinstance(ast, Add):
-        return calculate(ast.a) + calculate(ast.b)
-    elif isinstance(ast, Sub):
-        return calculate(ast.a) - calculate(ast.b)
-    elif isinstance(ast, Mul):
-        return calculate(ast.a) * calculate(ast.b)
-    elif isinstance(ast, Div):
-        return calculate(ast.a) / calculate(ast.b)
-    elif isinstance(ast, Pow):
-        return calculate(ast.a) ** calculate(ast.b)
-    
-    else:
-        raise Exception('Calculate error: unknown MathAST node type')
+        elif isinstance(ast, Add):
+            return calc(ast.a) + calc(ast.b)
+        elif isinstance(ast, Sub):
+            return calc(ast.a) - calc(ast.b)
+        elif isinstance(ast, Mul):
+            return calc(ast.a) * calc(ast.b)
+        elif isinstance(ast, Div):
+            return calc(ast.a) / calc(ast.b)
+        elif isinstance(ast, Pow):
+            return calc(ast.a) ** calc(ast.b)
+        
+        elif isinstance(ast, Variable):
+            if ast.name in kwargs:
+                return kwargs[ast.name]
+            else:
+                raise Exception('Calculate error: no value provided for variable: '+ast.name)
+
+        else:
+            raise Exception('Calculate error: unknown MathAST node type')
+    return calc(ast)
+
+if __name__ == "__main__":
+    print(calculate(parse('(4+6/5)-3.5'), x=4, a=3.5))
